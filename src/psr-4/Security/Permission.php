47d343afbcbb3d29e12d46288f3cb66c0aa1d5a9
sandersuarez <sandersuarezlara@gmail.com>
2022-05-15 18:18:57 +0200

    Fix and optimize the permission level getter function for the database

--- src/psr-4/Security/Permission.php
+++ src/psr-4/Security/Permission.php
@@ -12,29 +12,29 @@ class Permission
         if (isset($_SESSION['id']) && isset($_SESSION['key']) && isset($_SESSION['last_access'])) {
 
             // Session time verification
-            if (time() - $_SESSION['last_access'] > 60 * $_SESSION['inactive_time']) {
-                return -1;
+            if ((time() - $_SESSION['last_access']) > (60 * $_SESSION['inactive_time'])) {
+                return array('time' => 'Tiempo de sesión expirado');
             } else {
                 $pdo = Connection::getInstance();
                 $query = $pdo->prepare("SELECT * FROM usuarios WHERE id_usuario = :id");
-                $query->bindParam(':id', $_SESSION['id'], PDO::PARAM_STR);
+                $query->bindParam(':id', $_SESSION['id'], PDO::PARAM_INT);
                 $query->execute();
 
                 // Password verification
                 if ($result = $query->fetch(PDO::FETCH_ASSOC)) {
-                    if (password_verify($_SESSION['key'], $result['keyuser'])) {
+                    if (md5($_SESSION['key']) == $result['keyuser']) {
                         $_SESSION['last_access'] = time();
                         unset($result['keyuser']);
                         return array('user' => $result);
                     } else {
-                        return -2;
+                        return array('forbidden' => 'Zona restringida');
                     }
                 } else {
-                    return -2;
+                    return array('forbidden' => 'Zona restringida');
                 }
             }
         } else {
-            return -3;
+            return array('not_logged' => 'No se ha iniciado sesión');
         }
     }
 }
