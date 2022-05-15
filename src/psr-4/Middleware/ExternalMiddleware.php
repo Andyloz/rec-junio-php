50cbfa27293f720efd2c1f3bf13a3afe5a014430
sandersuarez <sandersuarezlara@gmail.com>
2022-05-15 22:52:11 +0200

    Add the external middleware

---
+++ src/psr-4/Middleware/ExternalMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FAFL\RecJunioPhp\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface;
+use FAFL\RecJunioPhp\Security\Permission;
+use FAFL\RecJunioPhp\VendorExtend\MyResponse;
+
+class ExternalMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        $permission = new Permission;
+        $level = $permission->getPermissionLevel();
+
+        if (!array_key_exists('user', $level)) {
+            $request = $request->withAttribute('level', $level);
+        } else {
+            return (new MyResponse())->withJson([
+                'error' => 'Ya hay una sesión abierta'
+            ]);
+        }
+
+        return $response;
+    }
+}
