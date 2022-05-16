e5cc237d75ebbb7b957116861e3b6ae506479a6b
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-16 22:06:43 +0200

    Added Schedule class to DataReadController->obtainSchedule

--- src/psr-4/Controller/DataReadController.php
+++ src/psr-4/Controller/DataReadController.php
@@ -3,6 +3,8 @@
 namespace FAFL\RecJunioPhp\Controller;
 
 use FAFL\RecJunioPhp\Data\Connection;
+use FAFL\RecJunioPhp\Data\Schedule\Schedule;
+use FAFL\RecJunioPhp\Data\Schedule\ScheduleRow;
 use FAFL\RecJunioPhp\VendorExtend\MyResponse;
 use Psr\Http\Message\ResponseInterface;
 use PDO;
@@ -14,13 +16,30 @@ class DataReadController
     $data = ['msg' => 'El profesor ' . $userID . ' no tiene horario'];
 
     $pdo = Connection::getInstance();
-    $query = $pdo->prepare("SELECT * FROM horario_lectivo WHERE usuario = :username");
+    $query = $pdo->prepare("
+SELECT 
+    id_horario id,
+    usuario userId, 
+    dia day,
+    hora hour,
+    grupo groupId,
+    g.nombre groupName, 
+    aula classroomId,
+    a.nombre classroomName 
+FROM horario_lectivo
+JOIN aulas a ON a.id_aula = horario_lectivo.aula
+JOIN grupos g ON g.id_grupo = horario_lectivo.grupo
+WHERE usuario = :username AND dia BETWEEN 1 AND 5 AND hora BETWEEN 1 AND 7
+    ");
     $query->bindParam('username', $userID, PDO::PARAM_INT);
     $query->execute();
 
-    $result = $query->fetchAll();
-    if ($result) $data = ['schedule' => $result];
+    $rawRows = $query->fetchAll();
+    $scheduleRows = array_map(fn($row) => new ScheduleRow(...$row), $rawRows);
+    $schedule = new Schedule($scheduleRows);
 
-    return $response->withJson($data);
+    return $response->withJson([
+      'schedule' => $schedule,
+    ]);
   }
 }
