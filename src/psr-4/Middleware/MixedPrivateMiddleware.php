9d9526b0408c22c7386fe2de9637f0e6128c01e0
sandersuarez <sandersuarezlara@gmail.com>
2022-05-16 19:37:06 +0200

    Add the mixed private middleware

---
+++ src/psr-4/Middleware/MixedPrivateMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FAFL\RecJunioPhp\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface;
+use FAFL\RecJunioPhp\Security\Permission;
+use FAFL\RecJunioPhp\VendorExtend\MyResponse;
+use Slim\Routing\RouteContext;
+
+class MixedPrivateMiddleware
+{
+  public function __invoke(Request $request, RequestHandler $handler): ResponseInterface
+  {
+    $level = $request->getAttribute('level');
+    $args = RouteContext::fromRequest($request)->getRoute()->getArguments();
+
+    if ($level['user']['tipo'] == 'admin' || ($level['user']['tipo'] == 'normal' && $level['user']['id_usuario'] == $args['userID'])) {
+      $request = $request->withAttribute('level', $level);
+      $response = $handler->handle($request);
+    } else {
+      $response = (new MyResponse())->withJson(['forbidden' => 'No tiene permiso para acceder a este servicio']);
+    }
+
+    return $response;
+  }
+}
