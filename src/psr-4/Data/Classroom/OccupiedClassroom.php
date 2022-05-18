0f3e21cf7778f4d933faed8252567d1be4f1847b
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-18 23:42:27 +0200

    Added PHPDoc to OccupiedClassroom constructor

--- src/psr-4/Data/Classroom/OccupiedClassroom.php
+++ src/psr-4/Data/Classroom/OccupiedClassroom.php
@@ -2,8 +2,18 @@
 
 namespace FAFL\RecJunioPhp\Data\Classroom;
 
+use FAFL\RecJunioPhp\Data\Group\Group;
+use FAFL\RecJunioPhp\Data\User\User;
+
 class OccupiedClassroom extends ScheduleClassroom
 {
+  /**
+   * @param int $id
+   * @param string $name
+   * @param int[] $scheduleRowIds
+   * @param Group[] $groups
+   * @param User[] $users
+   */
   public function __construct(
     int          $id,
     string       $name,
