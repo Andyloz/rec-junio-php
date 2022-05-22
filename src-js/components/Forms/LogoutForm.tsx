d60cd27c3c398df2fbdbc0904286025c56a1f516
sandersuarez <sandersuarezlara@gmail.com>
2022-05-22 19:31:45 +0200

    Add the logout form component

---
+++ src-js/components/Forms/LogoutForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../Button'
+
+const LogoutForm = () => {
+  return (
+    <form method="post">
+      <Button type='submit'>Cerrar Sesión</Button>
+    </form>
+  )
+}
+
+export default LogoutForm
