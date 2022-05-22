64e5807cd4cdf7f812ffe1c25117c73f0d2dedb0
sandersuarez <sandersuarezlara@gmail.com>
2022-05-22 15:41:24 +0200

    Update the render react DOM method to use the react 18 api

--- src-js/index.tsx
+++ src-js/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
 const container = document.getElementById('root');
+const root = createRoot(container!);
 
 // render
-ReactDOM.render(
-  <div>Hello World</div>,
-  container
-);
+root.render(<App />);
