c166b776ebe4b5534e998f564939a36f6beeda00
sandersuarez <sandersuarezlara@gmail.com>
2022-05-22 20:24:54 +0200

    Add the welcome layer and the teacher selector form

--- src-js/components/Layout/Main.tsx
+++ src-js/components/Layout/Main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import LoginForm from '../Forms/LoginForm'
+import TeacherSelectorForm from '../Forms/TeacherSelectorForm'
+import WelcomeLayer from '../WelcomeLayer'
 
 const Main = () => {
   return (
     <main className="container px-3">
       <h2 className="mb-3 mt-5 text-center text-sm-start">Iniciar Sesión</h2>
       <LoginForm/>
+      <WelcomeLayer/>
+      <TeacherSelectorForm/>
     </main>
   )
 }
