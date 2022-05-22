d6ea4a793f9bff98ac81392121a21e65107a6505
sandersuarez <sandersuarezlara@gmail.com>
2022-05-22 15:42:06 +0200

    Add the layout main components

---
+++ src-js/components/Layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="container">
+        <h1>Práctica final PHP</h1>
+    </header>
+  )
+}
+
+export default Header;
