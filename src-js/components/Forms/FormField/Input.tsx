1de37a2a738c9e9a18f872c51d860e425fe557b3
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-23 21:36:54 +0200

    Added placeholder prop to Input

--- src-js/components/Forms/FormField/Input.tsx
+++ src-js/components/Forms/FormField/Input.tsx
@@ -4,11 +4,12 @@ interface IProps {
   type: 'text' | 'password'
   name: string
   maxLength?: number
+  placeholder?: string
 }
 
-const Input: FC<IProps> = ({ type, name, maxLength }) => {
+const Input: FC<IProps> = ({ type, name, maxLength, placeholder }) => {
   return (
-    <input type={type} className='form-control' id={name} name={name} maxLength={maxLength} />
+    <input type={type} className='form-control' id={name} name={name} maxLength={maxLength} placeholder={placeholder} />
   )
 }
 
