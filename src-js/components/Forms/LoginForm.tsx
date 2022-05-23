07788415d28826b39162ef86535182189c70968c
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-23 21:26:17 +0200

    Moved login title to LoginForm component

--- src-js/components/Forms/LoginForm.tsx
+++ src-js/components/Forms/LoginForm.tsx
@@ -4,11 +4,14 @@ import FormField from './FormField/FormField'
 
 const LoginForm = () => {
   return (
-    <form method='post'>
-      <FormField type={'text'} label='Usuario:' name={'login-username'} maxLength={20} />
-      <FormField type={'text'} label='Contraseña:' name={'login-password'} maxLength={6} />
-      <Button type='submit' level='btn-primary'>Iniciar Sesión</Button>
-    </form>
+    <>
+      <h2 className='mb-3 mt-5 text-center text-sm-start'>Iniciar Sesión</h2>
+      <form method='post'>
+        <FormField type={'text'} label='Usuario:' name={'login-username'} maxLength={20} />
+        <FormField type={'text'} label='Contraseña:' name={'login-password'} maxLength={6} />
+        <Button type='submit' level='btn-primary'>Iniciar Sesión</Button>
+      </form>
+    </>
   )
 }
 
