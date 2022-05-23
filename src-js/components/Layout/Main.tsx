ce342475a63892589fa2ac18f131d00f95e5e609
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-23 21:28:12 +0200

    Layout now occupies the entire available vertical space (removing all initial unused elements)

--- src-js/components/Layout/Main.tsx
+++ src-js/components/Layout/Main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import LoginForm from '../Forms/LoginForm'
-import TeacherSelectorForm from '../Forms/TeacherSelectorForm'
-import TeacherSchedule from '../TeacherSchedule'
-import WelcomeLayer from '../WelcomeLayer'
 
 const Main = () => {
   return (
-    <main className='container'>
+    <main className='flex-grow-1'>
       <LoginForm />
-      <WelcomeLayer />
-      <TeacherSelectorForm />
-      <h2>Su horario</h2>
-      <h2>Horario de los profesores</h2>
-      <TeacherSchedule />
     </main>
   )
 }
