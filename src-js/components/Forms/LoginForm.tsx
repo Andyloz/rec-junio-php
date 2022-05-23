14e8f3707f8e4fa4a11ab105837e88c0cc2062e9
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-23 21:53:29 +0200

    Reformed LoginForm

--- src-js/components/Forms/LoginForm.tsx
+++ src-js/components/Forms/LoginForm.tsx
@@ -4,14 +4,21 @@ import FormField from './FormField/FormField'
 
 const LoginForm = () => {
   return (
-    <>
-      <h2 className='mb-3 mt-5 text-center text-sm-start'>Iniciar Sesión</h2>
-      <form method='post'>
-        <FormField type={'text'} label='Usuario:' name={'login-username'} maxLength={20} />
-        <FormField type={'text'} label='Contraseña:' name={'login-password'} maxLength={6} />
-        <Button type='submit' level='btn-primary'>Iniciar Sesión</Button>
-      </form>
-    </>
+    <div className='container h-100 d-flex'>
+      <div className='m-auto d-flex flex-column flex-md-row align-items-center gap-5'>
+        <h2 className='text-center'>
+          <span className='display-1 d-md-none'>Iniciar sesión</span>
+          <span className='display-3 d-none d-md-block'>Iniciar sesión</span>
+        </h2>
+        <form className='card shadow-sm p-2' method='post'>
+          <div className='card-body'>
+            <FormField type={'text'} label='Usuario:' name={'login-username'} maxLength={20} />
+            <FormField type={'text'} label='Contraseña:' name={'login-password'} maxLength={6} />
+            <Button type='submit' level='btn-primary'>Iniciar Sesión</Button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
