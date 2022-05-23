d7e67850168870d557b9a4b4da2e794ee68a3f90
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-23 22:06:20 +0200

    Added submit handler

--- src-js/components/Forms/LoginForm.tsx
+++ src-js/components/Forms/LoginForm.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { FC, FormEventHandler } from 'react'
 import Button from '../Button'
 import FormField from './FormField/FormField'
 
-const LoginForm = () => {
+interface IProps {
+  onPressedLogin: (data: FormData) => void
+}
+
+const LoginForm: FC<IProps> = ({ onPressedLogin }) => {
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    onPressedLogin(formData)
+  }
+
   return (
     <div className='container h-100 d-flex'>
       <div className='m-auto d-flex flex-column flex-md-row align-items-center gap-5'>
@@ -10,10 +21,10 @@ const LoginForm = () => {
           <span className='display-1 d-md-none'>Iniciar sesión</span>
           <span className='display-3 d-none d-md-block'>Iniciar sesión</span>
         </h2>
-        <form className='card shadow-sm p-2' method='post'>
+        <form className='card shadow-sm p-2' onSubmit={ handleSubmit }>
           <div className='card-body'>
-            <FormField type={'text'} label='Usuario:' name={'login-username'} maxLength={20} />
-            <FormField type={'text'} label='Contraseña:' name={'login-password'} maxLength={6} />
+            <FormField type={ 'text' } label='Usuario:' name={ 'login-username' } maxLength={ 20 } />
+            <FormField type={ 'text' } label='Contraseña:' name={ 'login-password' } maxLength={ 6 } />
             <Button type='submit' level='btn-primary'>Iniciar Sesión</Button>
           </div>
         </form>
