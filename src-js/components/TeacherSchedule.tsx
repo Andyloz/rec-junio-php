ca9374af2d392d97d922746c97c5b604adb00f0e
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-24 15:06:26 +0200

    Added user prop

--- src-js/components/TeacherSchedule.tsx
+++ src-js/components/TeacherSchedule.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { FC } from 'react'
 import ScheduleTable from './Tables/ScheduleTable'
+import User from './shapes/User'
 
-const TeacherSchedule = () => {
+interface IProps {
+  user: User
+}
+
+const TeacherSchedule: FC<IProps> = ({ user }) => {
   return (
     <section>
-      <h3 className='mt-5'>Horario del profesor <span></span></h3>
+      <h3 className='mt-5'>Horario del profesor { user.nombre }</h3>
       <ScheduleTable />
     </section>
   )
