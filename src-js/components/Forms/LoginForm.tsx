5df2a95701ada1acb5e1d6344c6fbf280c11cb5e
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-24 14:55:03 +0200

    Added error messages to LoginForm and added useApi request

--- src-js/components/Forms/LoginForm.tsx
+++ src-js/components/Forms/LoginForm.tsx
@@ -1,12 +1,15 @@
 import React, { FC, FormEventHandler } from 'react'
 import Button from '../Button'
 import FormField from './FormField/FormField'
+import useMediaQuery from '../../hooks/useMediaQuery'
 
 interface IProps {
+  message?: string
+  error?: string
   onPressedLogin: (data: FormData) => void
 }
 
-const LoginForm: FC<IProps> = ({ onPressedLogin }) => {
+const LoginForm: FC<IProps> = ({ message, error, onPressedLogin }) => {
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
@@ -16,17 +19,31 @@ const LoginForm: FC<IProps> = ({ onPressedLogin }) => {
     }
   }
 
+  const displayError = typeof error === 'string'
+  const displayMessage = typeof message === 'string'
+  const showAnyMessage = displayError || displayMessage
+  const titleSizeMatches = useMediaQuery('(max-width: 550px)')
+
   return (
     <div className='container h-100 d-flex'>
-      <div className='m-auto d-flex flex-column flex-md-row align-items-center gap-5'>
-        <h2 className='text-center'>
-          <span className='display-1 d-md-none'>Iniciar sesión</span>
-          <span className='display-3 d-none d-md-block'>Iniciar sesión</span>
-        </h2>
+      <div className='m-auto d-flex flex-column flex-md-row align-items-center gap-3 gap-md-5'>
+        <div className='d-flex flex-column'>
+          <h2 className='text-center'>
+            <span className='display-1 d-md-none' style={ { fontSize: titleSizeMatches ? '50px' : undefined } }>Iniciar sesión</span>
+            <span className='display-3 d-none d-md-block'>Iniciar sesión</span>
+          </h2>
+          {
+            showAnyMessage &&
+            <div className='mt-2 mt-md-3 mx-4'>
+              { displayError && <div className='alert alert-danger' role='alert'>{ error }</div> }
+              { displayMessage && <div className='alert alert-warning' role='alert'>{ message }</div> }
+            </div>
+          }
+        </div>
         <form className='card shadow-sm p-2' onSubmit={ handleSubmit }>
           <div className='card-body'>
-            <FormField type={ 'text' } labelText='Usuario:' name={ 'login-username' } maxLength={ 20 } required />
-            <FormField type={ 'password' } labelText='Contraseña:' name={ 'login-password' } maxLength={ 6 } required />
+            <FormField type={ 'text' } labelText='Usuario:' name={ 'username' } maxLength={ 20 } required />
+            <FormField type={ 'password' } labelText='Contraseña:' name={ 'password' } maxLength={ 6 } required />
             <Button type='submit' level='btn-primary'>Iniciar Sesión</Button>
           </div>
         </form>
--- src-js/components/Layout/Main.tsx
+++ src-js/components/Layout/Main.tsx
@@ -1,23 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import LoginForm from '../Forms/LoginForm'
 import Dashboard from '../Dashboard'
+import useApi from '../../hooks/useApi'
+import Message from '../shapes/Message'
+import User from '../shapes/User'
+import Error from '../shapes/Error'
 
 const Main = () => {
+  const { response, doRequest } = useApi<{ user: User } | Error | Message>()
   const [user, setUser] = useState<{}>()
+  const [loginMessage, setLoginMessage] = useState<string>()
+  const [errorMessage, setLoginError] = useState<string>()
+
+  useEffect(() => {
+    if (response) {
+
+      if ('msg' in response) {
+        setLoginMessage(response.msg)
+      } else if ('error' in response) {
+        setLoginError(response.error)
+      } else { // is User
+        setUser(response.user)
+      }
+
+    }
+  }, [response])
+
+  const handleLoginPress = (fd: FormData) => {
+    const data = Object.fromEntries(fd.entries())
+    doRequest('api/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+  }
 
   return (
     <main className='flex-grow-1'>
       {
         user
           ? <Dashboard />
-          : <LoginForm
-            onPressedLogin={
-              (fd) => {
-                const form = Object.fromEntries(fd.entries())
-                console.log(form)
-                setUser({})
-              }
-            } />
+          : <LoginForm onPressedLogin={ handleLoginPress } message={ loginMessage } error={ errorMessage } />
       }
     </main>
   )
