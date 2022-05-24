a4b5c2f9f264c92833453aba3d5f5baac4edf708
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-24 15:55:46 +0200

    Added onPressedSubmitProp on TeacherSelectorForm

--- src-js/components/Forms/TeacherSelectorForm.tsx
+++ src-js/components/Forms/TeacherSelectorForm.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { FC, FormEventHandler } from 'react'
 import Button from '../Button'
 import Label from './FormField/Label'
 import Select from './FormField/Select'
 
-const TeacherSelectorForm = () => {
+interface IProps {
+  onPressedSubmit: (userId: number) => void
+}
+
+const TeacherSelectorForm: FC<IProps> = ({ onPressedSubmit }) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const stringUserId = formData.get('teacher-selector') as string
+    onPressedSubmit(parseInt(stringUserId))
+  }
+
   return (
-    <form className='d-flex flex-row flex-wrap align-items-center mt-4' method='get'>
+    <form className='d-flex flex-row flex-wrap align-items-center mt-4' onSubmit={ handleSubmit }>
       <Label idFor='teacher-selector' className='m-0 mb-2 me-4 mb-sm-0'>Seleccione el profesor</Label>
       <Select name='teacher-selector' className='mb-3 w-25 me-4 mb-sm-0'>
         <option value='1' selected>Apellido1 Apellido2 NOMBRE1</option>
