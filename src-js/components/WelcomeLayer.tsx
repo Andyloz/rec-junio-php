8cd9a3698f94073344b4131118cb15110aaf486d
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-25 12:51:57 +0200

    Removed component

--- src-js/components/WelcomeLayer.tsx
+++ src-js/components/WelcomeLayer.tsx
@@ -1,16 +1,22 @@
 import React, { FC } from 'react'
-import LogoutForm from './Forms/LogoutForm'
 import User from './shapes/User'
+import useApi from '../hooks/useApi'
 
 interface IProps {
   user: User
 }
 
 const WelcomeLayer: FC<IProps> = ({ user }) => {
+  const { response, doRequest } = useApi<{ msg: string }>()
+
   return (
     <div className='d-flex flex-row flex-wrap align-items-center mt-4'>
       <p className='m-0 mb-2 me-4 mb-sm-0'>Bienvenido, <span>{ user.usuario }</span></p>
-      <LogoutForm />
+      <button
+        type='submit'
+        className='btn btn-secondary'
+        onClick={() => doRequest('api/logout', { method: 'POST' })}
+      >Cerrar Sesión</button>
     </div>
   )
 }
