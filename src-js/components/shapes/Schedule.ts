f30bf37906fd30e81b38193902750a973914d2a2
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-25 11:53:28 +0200

    interval always present

--- src-js/components/shapes/Schedule.ts
+++ src-js/components/shapes/Schedule.ts
@@ -5,7 +5,7 @@ export type Hour = 1 | 2 | 3 | 4 | 5 | 6 | 7
 
 interface Schedule  {
   [key: string]: {
-    [key: string]: ScheduleInterval | undefined
+    [key: string]: ScheduleInterval
   }
 }
 
