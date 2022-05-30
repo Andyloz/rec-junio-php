0c2df0fb2dcbe1ec1da3a1d0086925465739e555
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-30 23:09:21 +0200

    Removed useStateWithLabel

--- src-js/hooks/useSession.ts
+++ src-js/hooks/useSession.ts
@@ -1,5 +1,80 @@
-function useSession() {
+import { useApi, useApiWith } from './useApi'
+import User from '../components/shapes/User'
+import { useEffect, useRef, useState } from 'react'
+import Message from '../components/shapes/Message'
 
+export interface Login {
+  Request: { username: string, password: string }
+  Response: { user: User }
+    | { msg: string } | { error: string }
+}
+
+export interface Session {
+  Response: { user: User }
+    | { 'not-logged': string } | { 'error': string } | { 'forbidden': string } | { 'time': string }
+}
+
+export interface Logout {
+  Response: { msg: string }
+}
+
+function useSession() {
+  const { doRequest: doLoginRequest } = useApiWith.bodyParams<Login['Request'], Login['Response']>('api/login')
+  const { doRequest: doSessionRequest } = useApi<Session['Response']>('api/session-status')
+  const { doRequest: doLogoutRequest } = useApi<Logout>('api/logout', { method: 'POST' })
+
+  const [user, setUser] = useState<User>()
+  const [msg, setMsg] = useState<Message<'info' | 'warning' | 'error'>>()
+  const sessionInterval = useRef<number>()
+
+  const sessionClear =
+    () => {
+      setUser(undefined)
+      clearInterval(sessionInterval.current)
+      sessionInterval.current = undefined
+    }
+
+  const sessionCheck =
+    () => {
+      doSessionRequest().then(res => {
+        if ('user' in res) {
+          setUser(res['user'])
+        } else {
+          sessionClear()
+          if ('time' in res) {
+            setMsg({ content: res['time'], type: 'info' })
+          } else if ('forbidden' in res) {
+            setMsg({ content: res['forbidden'], type: 'error' })
+          }
+        }
+      })
+    }
+
+  useEffect(() => {
+    sessionCheck()
+    sessionInterval.current = window.setInterval(sessionCheck, 60000)
+    return sessionClear
+  }, [])
+
+  const login = (credentials: Login['Request']) => {
+    setMsg(undefined)
+    doLoginRequest(credentials).then(res => {
+      if ('msg' in res) {
+        setMsg({ content: res['msg'], type: 'warning' })
+      } else if ('error' in res) {
+        setMsg({ content: res['error'], type: 'error' })
+      } else { // user
+        setUser(res['user'])
+      }
+    })
+  }
+
+  const logout = () => doLogoutRequest()
+    .then(() => setUser(undefined))
+
+  const clearMsg = () => setMsg(undefined)
+
+  return { user, msg, login, logout, clearMsg }
 }
 
 export default useSession
--- src-js/hooks/useStateWithLabel.ts
+++
@@ -1,9 +0,0 @@
-import { useDebugValue, useState } from 'react'
-
-const useStateWithLabel = <S> (label: string, initialValue: S) => {
-  const [value, setValue] = useState<S>(initialValue)
-  useDebugValue(`${ label }: ${ value }`)
-  return [value, setValue]
-}
-
-export default useStateWithLabel
