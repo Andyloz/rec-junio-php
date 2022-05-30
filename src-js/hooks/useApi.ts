f38d974814d0576039115bec56a76773b1943f64
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-30 21:32:06 +0200

    Undo useApi2Base and removed old useApi

--- src-js/hooks/useApi.ts
+++ src-js/hooks/useApi.ts
@@ -1,30 +1,6 @@
 import { useState } from 'react'
 
-const useApi = <T>() => {
-  const [response, setResponse] = useState<T>()
-  const [pending, setPending] = useState(false)
-
-  const doRequest = async (url: string, requestProps: RequestInit = {}) => {
-    setPending(true)
-    const response = await fetch(url, {
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'same-origin',
-      ...requestProps,
-    })
-    const text = await response.text()
-    const object = JSON.parse(text)
-    setPending(false)
-    setResponse(object)
-  }
-
-  return { response, doRequest, pending }
-}
-
-export default useApi
-
-// useApi2
-
-export async function baseDoRequest<R>(url: string, requestProps: RequestInit = {}): Promise<R> {
+async function baseDoRequest<R>(url: string, requestProps: RequestInit = {}): Promise<R> {
   const response = await fetch(url, {
     headers: { 'Content-Type': 'application/json' },
     credentials: 'same-origin',
@@ -34,7 +10,7 @@ export async function baseDoRequest<R>(url: string, requestProps: RequestInit = 
   return JSON.parse(text) as R
 }
 
-export function useApi2Base<R>() {
+function useApi2Base<R>() {
   const [pending, setPending] = useState(false)
 
   function doRequest(url: string, requestProps: RequestInit = {}) {
@@ -60,8 +36,6 @@ export type ParametrizedUrl<P> = (params: P) => string
 export function buildParametrizedUrl<P>(url: TemplateStringsArray, ...tParams: Extract<keyof P, string>[]) {
   return (params: P) => tParams.reduce((prev, curr, i) => prev + params[curr] + url[i + 1], url[0])
 }
-
-const url = buildParametrizedUrl<{ id: number }>`https://api.example.com/users/${ 'id' }`
 
 export const useApi2With = {
   bodyParams<P, R = void>(url: string, requestProps: RequestInit = {}) {
