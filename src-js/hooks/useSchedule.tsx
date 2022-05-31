ae72e80c7bcd69713d70b9108a471a36d34f84a1
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-05-31 15:33:42 +0200

    Removed context abilities from useSchedule

--- src-js/hooks/useSchedule.tsx
+++ src-js/hooks/useSchedule.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, FC, ReactNode, useState } from 'react'
+import React, { useState } from 'react'
 import Schedule from '../components/shapes/Schedule'
 import { buildParametrizedUrl, useFetchWith } from './useFetch'
 import User from '../components/shapes/User'
@@ -6,13 +6,6 @@ import User from '../components/shapes/User'
 export interface GetSchedule {
   Request: { userId: number }
   Response: { schedule: Schedule }
-}
-
-const ScheduleContext = createContext<ReturnType<typeof useSchedule> | undefined>(undefined)
-
-export const ScheduleProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const context = useSchedule()
-  return <ScheduleContext.Provider value={ context } children={ children } />
 }
 
 export const useSchedule = () => {
@@ -34,10 +27,4 @@ export const useSchedule = () => {
   return { schedule, user, setUser, refreshData }
 }
 
-export const useScheduleContext = () => {
-  const context = useSchedule()
-  if (!context) {
-    throw new Error('Schedule context not initialized')
-  }
-  return context
-}
+export default useSchedule
