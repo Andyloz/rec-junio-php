dedb9c4ca3bb460b19bb1ae15abbdad9a8fdae15
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-01 14:58:43 +0200

    Removed Day and Number types

--- src-js/components/shapes/Schedule.ts
+++ src-js/components/shapes/Schedule.ts
@@ -1,8 +1,5 @@
 import ScheduleInterval from './ScheduleInterval'
 
-export type Day = 1 | 2 | 3 | 4 | 5
-export type Hour = 1 | 2 | 3 | 4 | 5 | 6 | 7
-
 interface Schedule  {
   [k: number]: {
     [k: number]: ScheduleInterval | {}
