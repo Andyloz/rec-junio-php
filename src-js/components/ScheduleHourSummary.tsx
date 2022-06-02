ff2343031abf702d604b9bea3230a6c671cf53e1
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-02 20:45:29 +0200

    Extracted formatted intervals to an external module

--- src-js/components/ScheduleHourSummary.tsx
+++ src-js/components/ScheduleHourSummary.tsx
@@ -6,6 +6,7 @@ import useGroupsOp, { GroupActionMsg } from '../hooks/useGroupsOp'
 import ScheduleInterval from './shapes/ScheduleInterval'
 import User from './shapes/User'
 import useClassroomsGroups from '../hooks/useClassesGroups'
+import { fDays, fHourIntervals, fHours } from '../formattedIntervals'
 
 interface IProps {
   msg?: GroupActionMsg
@@ -17,33 +18,6 @@ interface IProps {
   classrooms: Exclude<ReturnType<typeof useClassroomsGroups>['classrooms'], undefined>
   onRmGroupPress: ReturnType<typeof useGroupsOp>['rmGroup']
   onAddPressed: ReturnType<typeof useGroupsOp>['addGroup']
-}
-
-const fHourIntervals: { [k: number]: string } = {
-  1: '8:15 - 9:15',
-  2: '9:15 - 10:15',
-  3: '10:15 - 11:15',
-  4: '11:15 - 11:45',
-  5: '11:45 - 12:45',
-  6: '12:45 - 13:45',
-  7: '13:45 - 14:45',
-}
-
-const fHours: { [k: number]: string } = {
-  1: '1º hora',
-  2: '2º hora',
-  3: '3º hora',
-  5: '4º hora',
-  6: '5º hora',
-  7: '6º hora',
-}
-
-const fDays: { [k: number]: string } = {
-  1: 'Lunes',
-  2: 'Martes',
-  3: 'Miércoles',
-  4: 'Jueves',
-  5: 'Viernes',
 }
 
 const ScheduleHourSummary: FC<IProps> = (
---
+++ src-js/formattedIntervals.ts
@@ -0,0 +1,26 @@
+export const fHourIntervals: { [k: number]: string } = {
+  1: '8:15 - 9:15',
+  2: '9:15 - 10:15',
+  3: '10:15 - 11:15',
+  4: '11:15 - 11:45',
+  5: '11:45 - 12:45',
+  6: '12:45 - 13:45',
+  7: '13:45 - 14:45',
+}
+
+export const fHours: { [k: number]: string } = {
+  1: '1º hora',
+  2: '2º hora',
+  3: '3º hora',
+  5: '4º hora',
+  6: '5º hora',
+  7: '6º hora',
+}
+
+export const fDays: { [k: number]: string } = {
+  1: 'Lunes',
+  2: 'Martes',
+  3: 'Miércoles',
+  4: 'Jueves',
+  5: 'Viernes',
+}
